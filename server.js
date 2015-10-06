727e0dade8def5a2ba500ae92f2441c87c7f1131
patmood <patrick.n.moody@gmail.com>
2015-10-05 22:32:50 -0700

    serve up index template

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+import fs from 'fs'
 import Promise from 'bluebird'
+Promise.promisifyAll(fs)
 global.Promise = Promise
 import koa from 'koa'
+import route from 'koa-route'
 
 const app = koa()
 
+// Log request times
 app.use(function * (next) {
   const start = process.hrtime()
   yield next
   console.log(process.hrtime(start), this.request.path)
 })
+
+// Serve up index template
+app.use(route.get('/', function * () {
+  this.body = fs.createReadStream('index.html', 'utf8')
+}))
 
 app.use(function * (next) {
   const {request, response} = this
