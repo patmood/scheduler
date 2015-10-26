bb1043ba98aebda331bb36384ef7ca08ff723b88
patmood <patrick.n.moody@gmail.com>
2015-10-26 14:57:39 -0700

    cleanup

--- libs/View.js
+++ libs/View.js
@@ -21,16 +21,8 @@ View.prototype._read = function () {}
 View.prototype.render = function * () {
   // push the <head> immediately
   this.push('<!DOCTYPE html><html><head><title>Hello World</title></head>')
-
-  // render the <body> on the next tick
-  var body = yield function (done) {
-    setImmediate(function () {
-      done(null, '<div id="app"></div><script src="/bundle.js"></script>')
-    })
-  }
-
   this.push('<body>')
-  this.push(body)
+  this.push('<div id="app"></div><script src="/bundle.js"></script>')
 
   // Push data here
   const s = db.journalEntryReader()
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -4,37 +4,35 @@ import Day from './Day'
 import SelectUser from './SelectUser'
 import AddUser from './AddUser'
 import { addUser, selectUser, deleteUser } from '../actions'
+import { partial } from 'lodash'
 
 export class App extends Component {
 
   render () {
     // console.log(this.props)
-    const { users, days, activeUser, onSelectUser, onAddUser } = this.props
+    const { users, days, activeUser, selectUser, addUser, deleteUser } = this.props
     const dayList = days
       .sort((a, b) => new Date(a.date) - new Date(b.date))
-      .map((day, i) => {
-        return <Day key={i}
-                    day={day}
-                    user={this.getUser(day.user_id, users)}
-                    highlight={day.user_id === activeUser}/>
-      })
+      .map((day) => Object.assign({}, day, {user: this.getUser(day.user_id, users)}))
+
     return (
       <div>
         <h1>Hello from App component</h1>
         <div>Active User: {activeUser}</div>
-        <SelectUser users={users} selectUser={onSelectUser} />
-        <AddUser addUser={onAddUser} />
-        <button onClick={this.triggerDelete.bind(this)}>Delete This User</button>
+        <SelectUser {...{users, selectUser}} />
+        <AddUser addUser={addUser} />
+        <button onClick={partial(deleteUser, activeUser)} disabled={!activeUser} >Delete This User</button>
         <h2>Schedule:</h2>
-        <div>{dayList}</div>
+        <div>
+          {dayList.map((day, i) =>
+            <Day key={i}
+              day={day}
+              user={day.user}
+              highlight={day.user_id === activeUser}/>
+          )}
+        </div>
       </div>
     )
-  }
-
-  triggerDelete () {
-    if (!this.props.activeUser) return
-    console.log(`deleting: ${this.props.activeUser}`)
-    this.props.onDeleteUser(this.props.activeUser)
   }
 
   getUser (id, userList) {
@@ -51,19 +49,13 @@ App.propTypes = {
   onDeleteUser: PropTypes.func,
 }
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.users,
-    days: state.days,
-    activeUser: state.activeUser,
-  }
-}
+const mapStateToProps = (state) => state
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSelectUser: (id) => dispatch(selectUser(id)),
-    onAddUser: (name) => dispatch(addUser(name)),
-    onDeleteUser: (id) => dispatch(deleteUser(id)),
+    selectUser: (id) => dispatch(selectUser(id)),
+    addUser: (name) => dispatch(addUser(name)),
+    deleteUser: (id) => dispatch(deleteUser(id)),
   }
 }
 
