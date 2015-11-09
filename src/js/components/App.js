dd18ad017654c5ac489e3be3486c3e90815bb63e
patmood <patrick.n.moody@gmail.com>
2015-11-08 22:53:33 -0800

    map props in separate file

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,10 +3,9 @@ import { connect } from 'react-redux'
 import Day from './Day'
 import SelectUser from './SelectUser'
 import AddUser from './AddUser'
-import { addUser, selectUser, deleteUser, assignDay, assignUnavailability, unassignUnavailability } from '../actions'
+import mapDispatchToProps from '../mapDispatchToProps'
 import { partial, where, values } from 'lodash'
 import moment from 'moment'
-import request from 'superagent'
 
 export class App extends Component {
   constructor () {
@@ -86,37 +85,7 @@ App.propTypes = {
   unassignUnavailability: PropTypes.func,
 }
 
-// QUESTION: Why cant props stay immutable?
 const mapStateToProps = (state) => state.toJS() // Convert from immutable.js to vanilla js object
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    selectUser: (id) => {
-      dispatch(selectUser(id))
-    },
-    addUser: (name) => {
-      // Optimistic action dispatch
-      const journalEntry = addUser(name)
-      dispatch(journalEntry, { optimistic: true }) // -> add extra attribute to say optimistic
-        // Need middleware ot handle optimistic option and undo it
-
-      // Server request with journal entry from action above
-      request.post('/journal')
-        .send(journalEntry)
-        .end((err, res) => {
-          if (err) console.log(err)
-          console.log(res)
-        })
-
-      // Update UI to apply or reverse update depending on server response
-    },
-    deleteUser: (id) => dispatch(deleteUser(id)),
-    assignDay: (date, userId) => dispatch(assignDay(date, userId)),
-    assignUnavailability: (date, userId) => dispatch(assignUnavailability(date, userId)),
-    unassignUnavailability: (unavailabilityId) => dispatch(unassignUnavailability(unavailabilityId)),
-  }
-}
-
 
 export default connect(
   mapStateToProps,
---
+++ src/js/mapDispatchToProps.js
@@ -0,0 +1,32 @@
+import request from 'superagent'
+import { addUser, selectUser, deleteUser, assignDay, assignUnavailability, unassignUnavailability } from './actions'
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+    selectUser: (id) => {
+      dispatch(selectUser(id))
+    },
+    addUser: (name) => {
+      // Optimistic action dispatch
+      const journalEntry = addUser(name)
+      dispatch(journalEntry, { optimistic: true }) // -> add extra attribute to say optimistic
+        // Need middleware ot handle optimistic option and undo it
+
+      // Server request with journal entry from action above
+      request.post('/journal')
+        .send(journalEntry)
+        .end((err, res) => {
+          if (err) console.log(err)
+          console.log(res)
+        })
+
+      // Update UI to apply or reverse update depending on server response
+    },
+    deleteUser: (id) => dispatch(deleteUser(id)),
+    assignDay: (date, userId) => dispatch(assignDay(date, userId)),
+    assignUnavailability: (date, userId) => dispatch(assignUnavailability(date, userId)),
+    unassignUnavailability: (unavailabilityId) => dispatch(unassignUnavailability(unavailabilityId)),
+  }
+}
+
+export default mapDispatchToProps
